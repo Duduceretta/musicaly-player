961da999a8ab815fbbc818c54127fbcbb7dbea9a
Eduardo Ceretta <duduceretta@gmail.com>
2025-05-10 17:40:50 -0300

    Ajustes para deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 5000;
@@ -14,8 +17,15 @@ app.get("/api/", (request, response) => {
 app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
+
 app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../../front-end/dist")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
