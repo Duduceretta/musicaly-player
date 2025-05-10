0c534a7a2a4d93f3a224b494564757650c0884e5
Eduardo Ceretta <duduceretta@gmail.com>
2025-05-10 18:10:28 -0300

    fix deploy

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Artists from "./pages/Artists";
 import Artist from "./pages/Artist";
 import Songs from "./pages/Songs";
