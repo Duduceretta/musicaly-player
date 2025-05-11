fb8939fd08d4e2300077abbf43941e8dc841acca
Eduardo Ceretta <duduceretta@gmail.com>
2025-05-10 21:30:15 -0300

    debug

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
-import "dotenv/config";
+// import "dotenv/config";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:5000/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = NODE_ENV === "development" ? "http://localhost:5000/api" : "/api";
+
+const URL = import.meta.env.DEV ? "http://localhost:5000/api" : "/api";
 
 const artistsResponse = await axios.get(`${URL}/artists`);
 const songsResponse = await axios.get(`${URL}/songs`);
